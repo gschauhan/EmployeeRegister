53f72808cd96c202991f752318f4d39a0e91b9e2
Gaurav Chauhan <gaurav.chauhan36@gmail.com>
2018-03-06 13:44:01 +0530

    designed employee create form

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule} from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { EmployeesComponent } from './employees/employees.component';
@@ -16,7 +16,7 @@ import { EmployeeListComponent } from './employees/employee-list/employee-list.c
     EmployeeListComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule, FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
